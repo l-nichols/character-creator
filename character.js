14500ebe52efdd0ff8bf76a83ed4627d4dd47338
stareyedgirl <stareyedgirl@gmail.com>
2015-03-22 18:36:04 -0500

    Added To-Do List, Began Trait List
    Added todo.txt
    Started the traits array that will hold the character traits to be
    randomized

---
+++ character.js
@@ -0,0 +1,45 @@
+var traits = {
+	'Creativity' : {
+		'Creative' : 
+			'Someone who is original, ingenious, or adaptive. </br>
+			This person might come up with a new way to look at an old problem, make interesting art, or invent something incredible.',
+		'Unimaginative' : 
+			'Someone who is uninspired or unable to adapt. </br>
+			This person doesn\'t just dislike thinking outside the box.  They\'re completely unable to do so.'
+	},
+	'Curiosity' : {
+		'Curious' : 'Someone who is interested in new things and open to new experiences.',
+		'Disinterested' : 'Someone who doesn\'t care about novelty and has no interest in new experiences or trying new things. </br>
+		Or perhaps someone who has stopped seeking new experiences and things or someone world-weary for whom nothing at all is new.'
+	},
+	'Open-Mindedness' : {
+		'Open-Minded' : 'Someone who has good judgement and the ability to think critically about issues and decisions without allowing biases or preconceived notions to affect decisions and conclusions.',
+		'Closed-Minded' : 'Someone who is prejudiced or dogmatic. </br>
+		This person allows personally held beliefs to affect their decision-making abilities and judgement.'
+	},
+	'Love of Learning' : {
+		'A Life-long Learner' : 'Someone who actively seeks out new skills and knowledge.',
+		'Resistant to Learning' : 'Someone who dislikes and avoids learning new skills and knowledge.'
+	},
+	'Wisdom' : {
+		'Wise' : 'Someone who uses their cumulative knowledge and experience to evaluate matters and make sense of them. </br>
+		A person who is wise does not just have knowledge and experiences, but ',
+		'Foolish' : 'Someone who does not learn from experience.  </br>
+		A foolish person may have knowledge and experience, but they are unable to use either one to inform their actions or advise others.'
+	},
+	'Bravery' : {
+		'Brave' : 'Someone who does what needs to be done despite being afraid.',
+		'Cowardly' : 'Someone for whom fear keeps them from accomplishing what they wish to accomplish or from doing the right thing because they are afraid of the consequences. </br>
+		Many people have specific fears or phobias, but a cowardly person may have many fears that stop them from doing many things.  Or they may choose to avoid standing up for what is right when presented with a trying situation.'
+	},
+	'Persistence' : {
+		'Persistent' : 'Someone who perseveres and finishes what they start despite obstacles.',
+		'A Quitter' : 'Someone who is faint-hearted and gives up at the first sign of difficulty.
+		A person who is a quitter may lack confidence in their ability to overcome obstacles, or they may simply not wish to expend the effort necessary.'
+	},
+	'Integrity' : {
+		'Brave' : 'Someone who does what needs to be done despite being afraid.',
+		'Cowardly' : 'Someone for whom fear keeps them from accomplishing what they wish to accomplish or from doing the right thing because they are afraid of the consequences. </br>
+		Many people have specific fears or phobias, but a cowardly person may have many fears that stop them from doing many things.  Or they may choose to avoid standing up for what is right when presented with a trying situation.'
+	},
+}
