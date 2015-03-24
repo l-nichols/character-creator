df966b1aac362056894192d7a9ad283ad0a3c777
stareyedgirl <stareyedgirl@gmail.com>
2015-03-24 15:01:00 -0500

    Refactored and Removed Extra Parts
    Removed trait Life-long learner because it was a subset of curious and
    the redundancy was making the character creation function more
    complicated than it needed to be.
    
    Refactored the character creation function accordingly

--- character.js
+++ character.js
@@ -35,7 +35,7 @@ var positiveTraits = [
 	['Creative', 'Someone who is original, ingenious, or adaptive.'],
 	['Curious', 'Someone who is interested in new things and open to new experiences.'],
 	['Open-Minded', 'Someone who is receptive to and willing to actively seek out evidence against their favored beliefs.'],
-	['A Life-long Learner', 'Someone who actively seeks out new skills and knowledge.'],
+	//['A Life-long Learner', 'Someone who actively seeks out new skills and knowledge.'],
 	['Wise', 'Someone who uses their cumulative knowledge and experience to evaluate matters and make sense of them.'],
 	['Persistent', 'Someone who perseveres and finishes what they start despite obstacles.'],
 	['Brave', 'Someone who does what needs to be done despite being afraid.'],
@@ -60,7 +60,7 @@ var negativeTraits = [
 	['Unimaginative', 'Someone who is uninspired or unable to adapt. Someone who cannot think outside the box.'],
 	['Disinterested', 'Someone who doesn\'t care about novelty and has no interest in new experiences or trying new things. Or perhaps someone world-weary for whom nothing at all is new.'],
 	['Closed-Minded', 'Someone who displays a tendency to think in ways that favor their current beliefs and who will discard evidence that conflicts with their established beliefs.'],
-	['Resistant to Learning', 'Someone who dislikes and avoids learning new skills and knowledge.'],
+	//['Resistant to Learning', 'Someone who dislikes and avoids learning new skills and knowledge.'],
 	['Foolish', 'Someone who does not learn from experience.'],
 	['A Quitter', 'Someone who is faint-hearted and gives up at the first sign of difficulty.'],
 	['Cowardly', 'Someone for whom fear keeps them from accomplishing what they wish to accomplish or from doing the right thing because they are afraid of the consequences.'],
@@ -95,53 +95,15 @@ function createCharacter(positive, negative) {
 	for(var i=0; i < positive; i++){
 		index = positiveArray.length * Math.random() << 0;
 		character.push(positiveArray[index]);
-		if (positiveArray[index][0] == "Curious" || positiveArray[index][0] == "A Life-long Learner") {
-			trigger = positiveArray[index][0];
-			positiveArray.splice(index, 1);
-			negativeArray.splice(index, 1);
-			if (trigger == "Curious") {
-				console.log("Curious was trigger")
-				newIndex = getIndexOf(positiveArray, "A Life-long Learner");
-				positiveArray.splice((newIndex[0]), 1);
-				newIndex = getIndexOf(negativeArray, "Resistant to Learning");
-				negativeArray.splice((newIndex[0]), 1);
-			}
-			else {
-				console.log("Life-long Learner was trigger");
-				newIndex = getIndexOf(positiveArray, "Curious");
-				positiveArray.splice((newIndex[0]), 1);
-				newIndex = getIndexOf(negativeArray, "Disinterested");
-				negativeArray.splice((newIndex[0]), 1);
-			}
-		}
-    	else {
-    		positiveArray.splice(index, 1);
-    		negativeArray.splice(index, 1);
-    	}
+    	positiveArray.splice(index, 1);
+    	negativeArray.splice(index, 1);
 	}
 
 	for(var i=0; i < negative; i++){
 		index = negativeArray.length * Math.random() << 0;
     	character.push(negativeArray[index]);
-    	if (negativeArray[index][0] == "Resistant to Learning" || negativeArray[index][0] == "Disinterested") {
-    		trigger = negativeArray[index][0];
-    		positiveArray.splice(index, 1);
-    		negativeArray.splice(index, 1);
-    		if (trigger == "Resistant to Learning") {
-    			console.log("Resistant to learning was trigger");
-    			newIndex = getIndexOf(negativeArray, "Disinterested");
-    			negativeArray.splice((newIndex[0]), 1);
-    		}
-    		else {
-    			console.log("Disinterested was trigger");
-    			newIndex = getIndexOf(negativeArray, "Resistant to Learning");
-    			negativeArray.splice((newIndex[0]), 1);
-    		}
-    	}
-    	else {
-    		positiveArray.splice(index, 1);
-    		negativeArray.splice(index, 1);
-    	}
+    	positiveArray.splice(index, 1);
+    	negativeArray.splice(index, 1);
 	}
 	return character;
 };
